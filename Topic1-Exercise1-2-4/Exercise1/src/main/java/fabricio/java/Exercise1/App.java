b5f5381af4e0fa9aad2a8afefe3f244ac150f4b5
fabri3089 <fabi_3089@hotmail.com>
2016-01-18 12:02:39 -0300

    Exercises 1-2-4 Topic1

---
+++ Topic1-Exercise1-2-4/Exercise1/src/main/java/fabricio/java/Exercise1/App.java
@@ -0,0 +1,13 @@
+package fabricio.java.Exercise1;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
