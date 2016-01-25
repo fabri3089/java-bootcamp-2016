c8582aa56fa4ac69aec8af529dff33edb6257efd
fabri3089 <fabi_3089@hotmail.com>
2016-01-24 23:46:18 -0300

    Exercise 2.1

---
+++ Topic2/Exercise2.1/src/main/java/fabricio/java/Exercise2/Author.java
@@ -0,0 +1,24 @@
+package fabricio.java.Exercise2;
+
+public class Author {
+	private String nameAuthor;
+	private String message;
+	public Author(String nameAuthor, String message) {
+		// TODO Auto-generated constructor stub
+	}
+	public Author() {
+		// TODO Auto-generated constructor stub
+	}
+	public String getNameAuthor() {
+		return nameAuthor;
+	}
+	public void setNameAuthor(String nameAuthor) {
+		this.nameAuthor = nameAuthor;
+	}
+	public String getMessage() {
+		return message;
+	}
+	public void setMessage(String message) {
+		this.message = message;
+	}
+}
---
+++ Topic2/Exercise2.1/src/test/java/fabricio/java/Exercise2/BlogTest.java
@@ -0,0 +1,55 @@
+package fabricio.java.Exercise2;
+
+import static org.easymock.EasyMock.createMock;
+import static org.easymock.EasyMock.expect;
+import static org.easymock.EasyMock.replay;
+import static org.junit.Assert.assertArrayEquals;
+import static org.junit.Assert.assertEquals;
+
+import java.util.ArrayList;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import junit.framework.TestCase;
+
+public class BlogTest {
+
+
+	private MessageServices messageServicesMock;
+	@Before
+	public void setup(){
+		
+		//Mocking
+		messageServicesMock=createMock(MessageServices.class);
+		//Stubing
+		expect(messageServicesMock.getAuthor(1)).andReturn(new Author("Fabricio","Mensaje1"));
+		expect(messageServicesMock.getAuthor(2)).andReturn(new Author("Fabricio","Mensaje2"));
+		expect(messageServicesMock.getAuthor(3)).andReturn(new Author("Fabricio","Mensaje3"));
+		replay(messageServicesMock);
+	}
+	
+	@Test
+	public void TestSaveMessage(){
+
+		Blog bl= new Blog();
+		bl.setMessageServices(messageServicesMock);
+		assertEquals(3,bl.saveMessage());
+	}
+	@Before
+	public void setup2(){
+		
+		//Mocking
+		messageServicesMock=createMock(MessageServices.class);
+		//Stubing
+		expect(messageServicesMock.getLastTenMessages()).andReturn(10);
+		
+		replay(messageServicesMock);
+	}
+	@Test
+	public void TestGetLastTenMessages(){
+		Blog bl = new Blog();
+		bl.setMessageServices(messageServicesMock);
+		assertEquals(10,bl.getLastTenMessages());
+	}
+}
